fd107515bb93f5437843d26ef3babbc049054f23
Paramjot Malhotra <52860537+defaultparam@users.noreply.github.com>
2024-11-25 03:58:38 +0530

    Updated links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../App.css";
 
 const Footer = () => {
   return (
-    <div className="ftr">&copy; A project by <a href='https://param373r.me/'>param373r</a>.</div>
+    <div className="ftr">&copy; A project by <a href='https://x.com/defaultparam'>defaultparam</a>.</div>
   )
 }
 
